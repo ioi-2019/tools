2200ffbbfb01bd737cc590cfa8114c7425357f7a
Ismayil Hasanov <ihasanov@code.edu.az>
2019-05-29 13:17:27 +0400

    telegram bot created

---
+++ htc2019bot.py
@@ -0,0 +1,49 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import csv
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
+# Create a location dictionary
+loc_file = 'location.csv'
+with open(loc_file, mode='r') as infile:
+    reader = csv.reader(infile)
+    loc_dict = {rows[0]:rows[1] for rows in reader}
+
+# Create Updater and Dispatcher object
+token='802082349:AAEfJy8nf7ebSXmztNqnVv27ipHVXvJL2as'
+updater = Updater(token)
+dispatcher = updater.dispatcher
+
+# Reply wrapper method
+def reply(bot, update, text):
+    bot.send_message(chat_id=update.message.chat_id, text=text)
+
+# A simple /start
+def start(bot, update):
+    reply(bot, update, "Use /location XX or /script AA BB commands.")
+
+# Get the laptop name from location
+def location(bot, update, args):    
+    if len(args) == 0:
+        reply(bot, update, "Location is not shown")
+    else:
+        key = args[0]
+        if key in loc_dict:
+            reply(bot, update, loc_dict[key])
+        else:
+            reply(bot, update, "Location doesn't exist")
+
+# Default method to catch unknown commands
+def unknown(bot, update):
+    reply(bot, update, "This command is not implemented.")
+
+# Register handlers
+unknown_handler = MessageHandler(Filters.command, unknown)
+start_handler = CommandHandler('start', start)
+location_handler = CommandHandler('location', location, pass_args=True)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(location_handler)
+dispatcher.add_handler(unknown_handler)
+
+# Start the bot
+updater.start_polling()
