82873c1214561c29563724cb84813b093ccc3c9a
Ziyaddin Sadigov <ziyaddinsadigov@gmail.com>
2019-07-26 12:27:58 +0400

    add refresh timer and redirection to personal tasks after assigning task

--- request-fulfillment/app/src/environments/environment.ts
+++ request-fulfillment/app/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiURL: 'http://127.0.0.1:3000'
+  apiURL: 'http://127.0.0.1:3000',
+  pendingTasksRefreshInterval: 10000,
+  completedTasksRefreshInterval: 10000
 };
