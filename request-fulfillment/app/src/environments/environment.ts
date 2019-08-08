649e5c78cbefb8a684433dda42bef7e1c235adde
Ziyaddin Sadigov <ziyaddinsadigov@gmail.com>
2019-08-08 08:48:04 +0400

    fix ordering and refresh timer

--- request-fulfillment/app/src/environments/environment.ts
+++ request-fulfillment/app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiURL: 'http://127.0.0.1:3000',
-  pendingTasksRefreshInterval: 10000,
-  completedTasksRefreshInterval: 10000
+  pendingTasksRefreshInterval: 5000,
+  completedTasksRefreshInterval: 5000
 };
